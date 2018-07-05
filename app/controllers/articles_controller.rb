56b84d9235bdf4b44c03b8552223337541f340f5
jtrtj <jt.r@live.com>
2018-07-05 13:42:22 -0600

    write validations test for comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     @article.save
     
     flash.notice = "Article '#{@article.title}' Created!"
-
+    
     redirect_to article_path(@article)
   end
   
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Comment, type: :model do
+  describe 'validations' do
+    it {should belong_to(:article)}
+    it {should validate_presence_of(:author_name)}
+    it {should validate_presence_of(:body)}
+  end
+end
