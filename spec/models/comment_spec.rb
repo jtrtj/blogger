27d41e26028ea918a41a2eb8a87b2611d6c1a590
jtrtj <jt.r@live.com>
2018-07-05 14:19:13 -0600

    add tests to validate associations between comment and article

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 describe Comment, type: :model do
   describe 'validations' do
-    it {should belong_to(:article)}
+
     it {should validate_presence_of(:author_name)}
     it {should validate_presence_of(:body)}
+  end
+
+  describe 'relationships' do
+    
+    it {should belong_to(:article)}
   end
 end
