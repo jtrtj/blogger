d8d6de4e4a9edda48587d2db4a435a04357843c6
jtrtj <jt.r@live.com>
2018-07-04 18:07:38 -0600

    make test for editing article feature

---
+++ spec/features/user_can_edit_existing_article_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'user edits an existing article' do
+  describe 'they link from articles index' do
+    describe 'they fill in a new title and body' do
+      it 'edits the selcted article' do
+        article = Article.create!(title: 'this will change', body: 'so will this')
+
+        visit "articles/#{article.id}"
+        click_link 'Edit'
+
+        expect(current_path).to eq("/articles/#{article.id}/edit")
+
+        fill_in 'article[title]', with: 'updated title'
+        fill_in 'article[body]', with: 'updated body'
+        click_on 'Edit Article'
+
+        expect(current_path).to eq("/articles")
+        expect(page).to have_content('article.title')
+      end
+    end
+  end
+end
