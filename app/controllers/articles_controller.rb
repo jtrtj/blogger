161613690a30bf30b5f201fee59a171022c2ef90
jtrtj <jt.r@live.com>
2018-07-04 19:21:41 -0600

    add flash to create action. test passes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
     
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
   
@@ -28,7 +30,7 @@ class ArticlesController < ApplicationController
     @article.update(article_params)
 
     flash.notice = "Article '#{@article.title}' Updated!"
-    
+
     redirect_to article_path(@article)
   end
 
